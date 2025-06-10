4cf17a8b764902331fe9b6666bb2d9e9d1081bfb
RobsonJrr <robsonsiqueira78@gmail.com>
2025-06-10 14:36:52 -0300

    Criando o cabeçalho principal do site

--- contact/urls.py
+++ contact/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from contact import views
 
-app_name ='contact'
+app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('<int:contact_id>/', views.contact, name='contact'), 
+    path('', views.index, name='index'),  
+    ]
