336ae650ec8e6cc1a2ae8d39516b498b970f38c4
RobsonJrr <robsonsiqueira78@gmail.com>
2025-06-11 15:00:14 -0300

    Usando request.method e request. POST para saber quando o formulário é postado

--- contact/urls.py
+++ contact/urls.py
@@ -4,8 +4,13 @@ from contact import views
 
 app_name = 'contact'
 
-urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'), 
+urlpatterns = [ 
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),  
-    ]
+
+    #CRUD
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+]
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,26 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from contact.models import Contact
+from django.db.models import Q
+
+def create(request):
+    post = request.POST
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+
+    context = {
+
+    }
+
+    print()
+    print(request.method)
+    print()
+
+    return render(
+        request,
+        'contact/create.html',
+    )
