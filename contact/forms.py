5488e305ae7ec8356ef0683e9391b558d152ccbb
RobsonJrr <robsonsiqueira78@gmail.com>
2025-06-12 15:58:24 -0300

    Validando campos do formulário com clean, clean_field e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -1,30 +1,68 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
 from . import models
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui',
+            }
+        ),
+        label='meu saco',
+        help_text='ajuda',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Escreva aqui',
+        # })
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #             }
+        #     )
+                
+        # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Primeiro nome igual o sobrenome',
+                    code='invalid'
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+                
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+            'first_name',
+            ValidationError(
+                'Digite um nome válido',
+                code='invalid'
+            )
+        )
+        
+        return first_name
